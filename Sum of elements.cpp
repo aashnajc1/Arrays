25d99a29bfb48b3000243f98e753f491e12b6836
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-23 10:49:13 +0530

    ,

---
+++ Sum of elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin>>size;
+    int arr[100];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    int sum = 0;
+    for(int i=0;i<size;i++){
+        sum = sum + arr[i];
+    }
+    cout << "Sum of the elements is " << sum << endl;
+    return 0;
+}
